9d07abfa068be5817ae7142ad04b7508c14473c4
Mehdi <work138000@gmail.com>
2024-10-05 17:05:18 +0330

    add new section by js

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,38 @@
-"use strict"
+const acontainer = document.querySelector(".acontainer");
+const formBox = document.querySelector("#formBox");
+const nextButton = document.getElementById("NexP");
+
+formBox.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const nameInput = e.target.querySelector("input[name='type']");
+  if (!nameInput.value) {
+    alert("Please Enter Your Name");
+    return;
+  }
+
+  setTimeout(() => {
+    acontainer.style.opacity = 0;
+    setTimeout(() => {
+      acontainer.remove();
+      const nextSection = document.querySelector("#nextSection");
+      if (nextSection) {
+        nextSection.scrollIntoView({ behavior: "smooth" });
+      }
+    }, 500);
+  }, 500);
+});
+
+nextButton.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  acontainer.style.opacity = 0;
+  setTimeout(() => {
+    acontainer.remove();
+    const nextSection = document.querySelector("#nextSection");
+    if (nextSection) {
+      nextSection.scrollIntoView({ behavior: "smooth" });
+    }
+  }, 500);
+});
+``;
