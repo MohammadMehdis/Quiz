3d33ce118f3f68f15979f8d64fd3dacb4d175614
Mehdi <work138000@gmail.com>
2024-10-05 15:59:11 +0330

    Finish Sec ui

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,9 @@ module.exports = {
       colors:{
         green:"rgba(0,70,67,70%)"
       },
+      boxShadow:{
+       custom: "0px 20px 50px -10px rgba(0, 0, 0, 0.15)" ,
+      },
     },
   },
   plugins: [],
